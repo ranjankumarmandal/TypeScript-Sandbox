a43958ddcc16a4730e9f668a394a7cb9b6164cf8
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-04-05 01:14:01 +0530

    static typing to Object or Reference Data Type variables

--- 01_basic_types.ts
+++ 01_basic_types.ts
@@ -10,4 +10,5 @@ let age: number;
 age = 23;
 
 // static typing to Object or Reference Data Type variables
-let arr: number[] = [1, 2, 3, 4, 5];
+let arr: number[] = [1, 2, 3, 4, 5]; // arr with static typing having all its elements with type number
+let arr1: any[] = [1, 2, true, 'hello']; // arr1 with static typing having all its elements with any type
