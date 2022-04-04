bbc1b3866837a5262aa68575da3daf2c5c4dc82f
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-04-05 01:31:05 +0530

    created tuple array

--- 02_tuples.ts
+++ 02_tuples.ts
@@ -1,2 +1,11 @@
 // tuple
 let person: [number, string, boolean] = [1, 'hello', true];
+
+// tuple array
+let employee: [number, string][];
+employee = [
+  [1, 'name'],
+  [1, 'name'],
+  [1, 'name'],
+  [1, 'name'],
+];
