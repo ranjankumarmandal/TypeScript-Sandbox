8ef977ce7d62fa76edbe14c9a327680bf9ee3896
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-04-05 01:47:37 +0530

    updated

--- 03_union.ts
+++ 03_union.ts
@@ -1,4 +1,4 @@
-// union - if you want a variable to hold more than one type, use concept of unions in typescript
+// union - if you want a variable to hold more than one type, use static types concept called unions in typescript
 let id1: number | string = 2;
 
 let id2: number | string;
