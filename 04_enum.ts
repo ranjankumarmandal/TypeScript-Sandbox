f92eca62522abf66d9eaa25b2e8bab4131c43902
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-04-05 10:53:16 +0530

    values of a emum content

--- 04_enum.ts
+++ 04_enum.ts
@@ -1,1 +1,8 @@
 // enum - it allows us to define a set of named constants either numeric or strings
+
+enum Direction1 {
+  Up, // its value is 0
+  Down, // its value is 1
+  Left, // its value is 2
+  Right, // its value is 3
+}
