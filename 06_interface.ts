fc9080c590e1018661bbdefce267a8a2e4b6f0c6
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-04-05 12:25:24 +0530

    interface - very very popular for statically typing Object literals in typescript

---
+++ 06_interface.ts
@@ -0,0 +1,13 @@
+// interface - This is very popular for statically typing Object literals in typescript
+
+interface empInterface {
+  id: number;
+  name: string;
+  isDev: boolean;
+}
+
+const emp: empInterface = {
+  id: 1,
+  name: 'Brad',
+  isDev: true,
+};
