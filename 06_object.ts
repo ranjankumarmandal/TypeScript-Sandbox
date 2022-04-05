ac6b7e9aabc6e86bac433070d2f70555aa8eda6e
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-04-05 11:15:52 +0530

    we can use type to make objects static typed in typescript

--- 06_object.ts
+++ 06_object.ts
@@ -2,3 +2,7 @@
 
 // Object Literal in JS
 let obj = { a: 1, b: 'hello', c: true };
+
+// Object Literal in TS
+type User = { id: number; name: string };
+const user: User = { id: 1, name: 'john' }; // object with type in typescript
