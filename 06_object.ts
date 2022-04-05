ade496091c2b33d369f3394f1012fca27a53e796
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-04-05 12:22:21 +0530

    using objects with interface in TS is very popular and effective

--- 06_object.ts
+++ 06_object.ts
@@ -6,3 +6,10 @@ let obj = { a: 1, b: 'hello', c: true };
 // Object Literal in TS
 type User = { id: number; name: string };
 const user: User = { id: 1, name: 'john' }; // object with type in typescript
+
+// Most popular way for using objects in TypeScript is using inerface
+interface UserInterface {
+  id: number;
+  name: string;
+}
+const user1: UserInterface = { id: 1, name: 'Ranjan' };
