34050289ad6e300035da0fb8ecddbc3cc89ec480
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-04-05 12:45:25 +0530

    classes in typescript completed

--- 09_class.ts
+++ 09_class.ts
@@ -1,1 +1,15 @@
 // class in typescript - it is used to create multiples objects with template created in class. It works with concept of prototypes.
+
+class Person {
+  id: number;
+  name: string;
+
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+}
+
+const ranjan = new Person(1, 'Ranjan');
+const kumar = new Person(2, 'Kumar');
+const mandal = new Person(3, 'Mandal');
